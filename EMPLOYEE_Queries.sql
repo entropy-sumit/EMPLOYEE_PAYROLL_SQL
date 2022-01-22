dca0e7ecbb51e2eddd44d9150468b6bf6a61de02
entropy-sumit <sumitr600@gmail.com>
2022-01-22 23:52:18 +0530

    [add] sumit added uc2 program

--- EMPLOYEE_Queries.sql
+++ EMPLOYEE_Queries.sql
@@ -1,3 +1,12 @@
 --QUERIES START HERE
 --UC1--
 Create database Payroll_services
+
+--UC2--
+Create table employee_payroll
+(
+   Id int identity primary key,
+   name varchar(Max) Not null,
+   salary money default 1000,
+   startDate DateTime default GetDate()
+)
